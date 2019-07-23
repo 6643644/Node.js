94efd353ee07b405fe041314b99d1848576cd968
MilesChien <q543165@gmail.com>
2019-07-23 15:43:49 +0800

    練習node js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-/** index */
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+//===========================================================================================
+// 透過http模此啟動web server服務
+//===========================================================================================
+var server,
+    port = 3000,
+    http = require('http'),
+    url = require('url');
 
-var handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
+// 載入request模組
+var request = require('request');
 
+server = http.createServer(function (req, res) {
+    path = url.parse(req.url);
+    res.writeHead(200, { 'Content-Type': ' application/json' });
+    switch (path.pathname) {
+        case '/all':
+            console.log('path:' + path.pathname);
+            request({
+                url: 'http://localhost:8090/channel/allUser',
+            }, function (error, response, body) {
+                res.end(body);
+            });
+            break;
+        case '/index': res.end('index page');
+            break;
+        default: res.end('path.pathname=' + path.pathname);
+            break;
+    }
+    //設定服務監聽localhost:3000(127.0.0.1/:3000)
+}).listen(port);
 
-server.start(router.route,handle);
+console.log('Server is running');
